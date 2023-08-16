d2ceb1cf811dec0203e4100e9076a926172c285e
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2023-08-16 11:07:01 -0300

    Create Desafio1.rb
    Primeiro desafio do Dio - Formação Ruby Developer.

---
+++ Desafio1.rb
@@ -0,0 +1,10 @@
+#Desafio 
+#Você precisa criar um programa simples de acesso a leitores em uma biblioteca e para isso o usuário deverá digitar seu nome e sobrenome, além de sua idade.
+
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp
+puts "Olá #{nome} #{sobrenome}! Seja bem vindo! Sua idade é #{idade}."
