99be9095c4de688aca2f1d70bb059326e48c2a85
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2023-08-28 12:33:58 -0300

    APP.rb

--- Desafio5/APP.rb
+++ Desafio5/APP.rb
@@ -2,7 +2,7 @@ require_relative 'produto'
 require_relative 'mercado'
 
 produto = Produto.new
-produto.nome = 'Tomate'
-produto.preco = 2.50
+produto.nome = 'Cebola'
+produto.preco = 1.35
 
 Mercado.new(produto.nome, produto.preco).comprar
