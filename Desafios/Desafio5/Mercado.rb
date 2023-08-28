d005f80e7faa1e36702101bcc00e3009dee67335
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2023-08-28 12:08:15 -0300

    Mercado.rb

---
+++ Desafios/Desafio5/Mercado.rb
@@ -0,0 +1,10 @@
+class Mercado
+  def initialize(prduto, preco)
+    @produto = produto
+    @preco = preco
+  end
+
+  def comprar 
+    puts "Você comprou o produto #{produto} no valor de #{preco}"
+  end
+end
