05ba2fa69fa578aae1962738ff2af244b6d86f35
Leandro Apolinário Leite <97578213+Leandroapolinario@users.noreply.github.com>
2023-09-01 10:33:40 -0300

    Desafio6.rb

---
+++ Desafio6.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Livro criado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Livro atualizado com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_url, notice: 'Livro excluído com sucesso.'
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :rating, :description)
+  end
+end
